87d419b032e26cedcdf71abd5cddfc1e0f420218
Graham <graham.moss@hotmail.com>
2022-04-19 15:51:00 -0400

    add mobile check for resume

--- components/Windows/Resume.tsx
+++ components/Windows/Resume.tsx
@@ -1,7 +1,11 @@
 function Resume() {
-  return (
-    <object data="/assets/pdfs/myResume.pdf" width={'100%'} height={'95%'}></object>
-  )
+  let isMobile: boolean = /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent)
+
+  if (isMobile) {
+    return (<p>PDF integration is not supported on Mobile :(</p>)
+  } else {
+    return (<object data="/assets/pdfs/myResume.pdf" width={'100%'} height={'95%'}></object>)
+  }
 }
 
 export default Resume
