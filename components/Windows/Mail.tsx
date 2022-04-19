d06a64c54ee78aebe00641c09721fc46d0a36912
Graham <graham.moss@hotmail.com>
2022-04-19 12:56:21 -0400

    continue mail component

--- components/Windows/Mail.tsx
+++ components/Windows/Mail.tsx
@@ -1,8 +1,45 @@
 import styles from '../../styles/WindowPages.module.css'
+import Image from 'next/image'
+import React from 'react';
 
 function Mail() {
+
+  const sendMail = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className={styles.mailContainer}><p>Under Construction</p></div>
+    <form className={styles.mailForm}>
+      <div className={styles.mailSend}>
+        <button className={styles.mailSendButton} onClick={sendMail}>
+          <Image src='/assets/images/send-mail.png' height={15} width={15} alt='Send' />
+          <p>Send</p>
+        </button>
+      </div>
+      <div className={styles.mailContent}>
+        <div className={styles.mailDetails}>
+          <div className={styles.mailHeader}>
+            Subject
+          </div>
+          <hr />
+          <div className={styles.mailSubject}>
+            <p>To:</p>
+            <div className={styles.mailReceipient}>Graham Moss</div>
+          </div>
+          <hr />
+          <div className={styles.mailSubject}>
+            <p>Subject:</p>
+            <input />
+          </div>
+          <hr />
+          <div className={styles.mailSubject}>
+            <p>From:</p>
+            <input />
+          </div>
+        </div>
+        <textarea className={styles.mailText} />
+      </div>
+    </form>
   )
 }
 
