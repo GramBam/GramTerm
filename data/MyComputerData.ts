b417758299230bb628ef9841416a92d970aaa56d
Graham <graham.moss@hotmail.com>
2022-05-07 16:02:25 -0400

    mobile styling fixes

--- data/MyComputerData.ts
+++ data/MyComputerData.ts
@@ -1,13 +1,14 @@
 let path = '/assets/images/my-computer/'
 
 export const MyComputerData: { title: string, img: string }[] = [
+  { title: 'BrickBreaker', img: path + 'brickbreak.png' },
   { title: 'Civilization', img: path + 'civ.png' },
   { title: 'Counter-Strike', img: path + 'CounterStrike.png' },
   { title: 'DOOM', img: path + 'doom.png' },
   { title: 'Microsoft Golf', img: path + 'microsoftgolf.png' },
   { title: 'Microsoft Office', img: path + 'MicrosoftOffice.png' },
-  { title: 'BrickBreaker', img: path + 'brickbreak.png' },
   { title: 'Need For Speed', img: path + 'NeedForSpeed.png' },
+  { title: 'Paint', img: path + 'paint.png' },
   { title: 'Solitaire', img: path + 'solitaire.png' },
-  { title: 'Starcraft', img: path + 'StarCraft.png' }
+  { title: 'Starcraft', img: path + 'StarCraft.png' },
 ]
